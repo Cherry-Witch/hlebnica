0234c65dba2adae678c113f5c15fb22c6261b9ea
Cherry-Witch <angelina1337swag@mail.ru>
2022-07-26 23:35:56 +0500

    Исправил вывод в консоль

--- oop1/Program.cs
+++ oop1/Program.cs
@@ -6,7 +6,7 @@ namespace oop1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Привет Мир!");
         }
     }
 }
