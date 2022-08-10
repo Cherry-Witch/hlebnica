50663fabe9a7fb474863326690efc3888a3f6f0f
Cherry-Witch <angelina1337swag@mail.ru>
2022-08-10 09:27:39 +0500

    Добавление класса библеотеки

---
+++ oop1/Library/Library.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop1.Library
+{
+    class BookLibrary
+    {
+        public List<Shelf> Shelfs;
+
+        public int ShelfCount => Shelfs.Count;
+
+        public BookLibrary()
+        {
+            Shelfs = new List<Shelf>();
+        }
+
+        public void ShelfsInLibrary()
+        {
+            Console.WriteLine($"Книжных полок - {Shelfs.Count}");
+        }
+    }
+}
