922e51b7c67b2978fabd8d83b9e5a6efeeb56efe
Cherry-Witch <angelina1337swag@mail.ru>
2022-09-04 18:59:06 +0500

    Рефакторинг if else

--- oop1/Program.cs
+++ oop1/Program.cs
@@ -6,76 +6,75 @@ namespace oop1
 {
     class Program
     {
+        static Book ManualSearch(Library.Library bookLibrary)
+        {
+            int output;
+            string input;
+
+            Console.Write("Введите номер шкафа: ");
+            input = Console.ReadLine();
+
+            if (!int.TryParse(input, out output))
+            {
+                Console.WriteLine("Нужно ввести число!");
+                return null;
+            }
+
+            if (output >= bookLibrary.ShelfCount)
+            {
+                Console.WriteLine("Не найден шкаф с таким номером!");
+                return null;
+            }
+
+            Shelf currentShelf = bookLibrary.Shelfs[output];
+            currentShelf.PrintInfo();
+            Console.Write("Введите номер книги: ");
+            input = Console.ReadLine();
+
+            if (!int.TryParse(input, out output))
+            {
+                Console.WriteLine("Нужно ввести число!");
+                return null;
+            }
+
+            if (output >= currentShelf.BooksCount)
+            {
+                Console.WriteLine("Не найдено нигги с таким номером!");
+                return null;
+            }
+
+            return currentShelf.Books[output];
+        }
+
         static void Main(string[] args)
         {
             Library.Library bookLibrary = LibraryParser.LoadLibrary();
 
             bool work = true;
+            Book selectedBook;
 
             while (work)
             {
-                int output;
-
-                Console.Write("Поиск книги ручной или по названию? [0|1]: ");
                 string input = Console.ReadLine();
-
-                if (int.TryParse(input, out output))
+                switch (input)
                 {
-                    if (output == 0)
-                    {
-                        Console.Write("Введите номер шкафа: ");
-                        input = Console.ReadLine();
+                    case "autosearch":
+                        break;
+                    case "search":
+                        selectedBook = ManualSearch(bookLibrary);
+                        if (selectedBook != null)
+                        {
+                            selectedBook.PrintInfo();
+                        }
 
-                        if (int.TryParse(input, out output))
-                        {
-                            if (output < bookLibrary.ShelfCount)
-                            {
-                                Shelf currentShelf = bookLibrary.Shelfs[output];
-                                currentShelf.PrintInfo();
-                                Console.Write("Введите номер книги: ");
-                                input = Console.ReadLine();
-                                if (int.TryParse(input, out output))
-                                {
-                                    if (output < currentShelf.BooksCount)
-                                    {
-                                        Book currentBook = currentShelf.Books[output];
-                                        currentBook.PrintInfo();
-                                        Console.WriteLine();
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("Ну нет такой книги с таким номером!");
-                                    }
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Нужно ввести число.");
-                                }
-                            }
-                            else
-                            {
-                                Console.WriteLine("Нет полки с таким номером.");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Нужно ввести число.");
-                        }
-                    }
-                    else if (output == 1)
-                    {
-                        Console.WriteLine("[Поиск книги по названию.]");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Введите однин из вариантов: 0 или 1.");
-                    }
+                        break;
+                    case "stop":
+                        work = false;
+                        break;
+                    default:
+                        break;
                 }
-                else
-                {
-                    Console.WriteLine("Нужно ввести число.");
-                }
-                work = input != "stop";
+
             }
 
         }
